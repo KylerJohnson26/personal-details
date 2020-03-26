1098809a16e5ab22d66413e8a08a42e0b06ec084
kylerjohnson26 <kylerljohnson@icloud.com>
2020-03-26 00:01:19 -0400

    removing boilerplate html and adding personal details component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { PersonalDetailsComponent } from './personal-details/personal-details.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PersonalDetailsComponent
   ],
   imports: [
     BrowserModule,
