22e4e61cc5cf4b9a2b63a180c048536f11f4b4fe
kylerjohnson26 <kylerljohnson@icloud.com>
2020-03-29 18:01:00 -0400

    WIP: adding scatter chart - need to update on changes and get the axes to render

--- src/app/personal-detail/details-store.service.ts
+++ src/app/personal-detail/details-store.service.ts
@@ -6,8 +6,7 @@ import { PersonalDetails } from './personal-detail/personal-details.model';
   providedIn: 'root'
 })
 export class DetailsStoreService implements OnDestroy {
-
-  private readonly initialState: PersonalDetails[] = [];
+  private readonly initialState: PersonalDetails[] = this.getSeedData();
 
   private detailsSubbject$: BehaviorSubject<PersonalDetails[]> = new BehaviorSubject<PersonalDetails[]>(this.initialState);
   readonly details$: Observable<PersonalDetails[]> = this.detailsSubbject$.asObservable();
@@ -20,5 +19,73 @@ export class DetailsStoreService implements OnDestroy {
   ngOnDestroy(): void {
     // avoid memory leaks
     this.detailsSubbject$.complete();
+  }
+
+  getSeedData(): PersonalDetails[] {
+    return [
+      {
+        name: 'Jason Bourne',
+        age: 33,
+        weight: 175,
+        friends: ['Marie', 'Alex']
+      },
+      {
+        name: 'Jason Bourne',
+        age: 44,
+        weight: 175,
+        friends: ['Marie', 'Alex', 'Tony']
+      },
+      {
+        name: 'Jason Bourne',
+        age: 18,
+        weight: 175,
+        friends: [
+          'Marie',
+          'Alex',
+          'Alex',
+          'Alex',
+          'Alex',
+          'Alex',
+          'Alex',
+          'Alex'
+        ]
+      },
+      {
+        name: 'Jason Bourne',
+        age: 33,
+        weight: 175,
+        friends: ['Marie', 'Alex']
+      },
+      {
+        name: 'Jason Bourne',
+        age: 26,
+        weight: 175,
+        friends: ['Marie', 'Alex', 'Marie', 'Alex', 'Gary', 'Shawn']
+      },
+      {
+        name: 'Jason Bourne',
+        age: 27,
+        weight: 175,
+        friends: ['Marie', 'Alex', 'Gary', 'Shawn']
+      },
+      {
+        name: 'Jason Bourne',
+        age: 33,
+        weight: 175,
+        friends: ['Marie', 'Alex']
+      },
+      {
+        name: 'Andrew Bourne',
+        age: 15,
+        weight: 175,
+        friends: ['Alex']
+      },
+      {
+        name: 'John Bourne',
+        age: 24,
+        weight: 175,
+        friends: ['Marie', 'Alex', 'Gary', 'Shawn', 'Jeff', 'Drew', 'Ashley']
+      }
+    ];
   }
 }
--- src/app/personal-detail/personal-detail/details-graphic/details-graphic.component.ts
+++ src/app/personal-detail/personal-detail/details-graphic/details-graphic.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit, Input, OnChanges, ChangeDetectionStrategy } from '@angular/core';
+import { Component, OnInit, Input, OnChanges, ChangeDetectionStrategy, ElementRef, SimpleChanges, AfterContentInit } from '@angular/core';
+
+import * as d3 from 'd3';
+import { PersonalDetails } from '../personal-details.model';
 
 @Component({
   selector: 'app-details-graphic',
@@ -6,19 +9,143 @@ import { Component, OnInit, Input, OnChanges, ChangeDetectionStrategy } from '@a
   styleUrls: ['./details-graphic.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class DetailsGraphicComponent implements OnInit, OnChanges {
+export class DetailsGraphicComponent implements AfterContentInit, OnChanges {
 
-  @Input() data: any;
+  @Input() data: PersonalDetails[];
+
+  margin = {
+    top: 40,
+    right: 40,
+    bottom: 40,
+    left: 40
+  };
+  svg;
+  hostElement: ElementRef;
+  xScale;
+  yScale;
+  xAxis;
+  yAxis;
+
+  width = 500 - this.margin.left - this.margin.right;
+  height = 500 - this.margin.top - this.margin.bottom;
 
   constructor(
-  ) { }
-
-  ngOnInit() {
-    console.log(this.data);
+    private elRef: ElementRef
+  ) {
+    this.hostElement = this.elRef.nativeElement;
   }
+
+  // ngOnInit() {
+  //   this.buildScatterChart();
+  // }
 
   ngOnChanges() {
-    console.log(this.data);
+    // this.updateScatterChart(this.data)
   }
+
+  ngAfterContentInit() {
+    console.log('afterContentInit')
+    this.buildScatterChart();
+  }
+
+  prepareScatterData() {
+    // sort data by age desc
+    return [...this.data.sort((a, b) => b.age - a.age)];
+  }
+
+  buildScatterChart() {
+    const scatterData = this.prepareScatterData();
+    this.setScatterChartDimensions();
+    this.setXScale(scatterData);
+    this.setYScale(scatterData);
+    this.drawScatterChart(scatterData);
+    this.setXAxis();
+    this.setYAxis();
+    this.drawXAxis();
+    this.drawYAxis();
+  }
+
+  updateScatterChart(scatterData: PersonalDetails[]) {
+    if (!this.svg) {
+      this.drawScatterChart(scatterData);
+      return;
+    }
+    console.log(scatterData.length);
+    this.svg = d3
+      .select('.scatter-chart')
+      .selectAll('.scatter')
+      .data(scatterData);
+  }
+
+  setXScale(scatterData: PersonalDetails[]) {
+    this.xScale = d3
+      .scaleLinear()
+      .domain(d3.extent(scatterData, d => d.friends.length))
+      .range([0, this.width]);
+  }
+
+  setYScale(scatterData: PersonalDetails[]) {
+    this.yScale = d3
+      .scaleLinear()
+      .domain(d3.extent(scatterData, d => d.age))
+      .range([this.height, 0]);
+  }
+
+  setScatterChartDimensions() {
+    this.svg = d3
+      .select('.scatter-chart').append('svg')
+      .attr('width', this.width + this.margin.right + this.margin.left)
+      .attr('height', this.height + this.margin.top + this.margin.bottom)
+      .append('g')
+      .attr('transform', `translate(${this.margin.left}, ${this.margin.top})`);
+  }
+
+  drawScatterChart(scatterData): void {
+    this.svg
+      .append('g')
+      .attr('class', 'scatter-points')
+      .selectAll('.scatter')
+      .data(scatterData)
+      .enter()
+      .append('circle')
+      .attr('class', 'scatter')
+      .attr('cx', d => this.xScale(d.friends.length))
+      .attr('cy', d => this.yScale(d.age))
+      .attr('r', 3)
+      .style('fill', '#FF4081')
+      .style('fill-opacity', 0.7);
+  }
+
+  setXAxis() {
+    this.xAxis = d3
+      .axisBottom(this.xScale)
+      // .tickFormat(formatTicks)
+      .tickSizeInner(-this.height)
+      .tickSizeOuter(0);
+  }
+
+  drawXAxis() {
+    this.svg
+      .append('g')
+      .attr('class', 'x axis')
+      .attr('transform', `translate(0, ${this.height})`)
+      .call(this.xAxis);
+  }
+
+  setYAxis() {
+    this.yAxis = d3
+      .axisLeft(this.yScale)
+      // .tickFormat(formatTicks)
+      .tickSizeInner(-this.height)
+      .tickSizeOuter(0);
+  }
+
+  drawYAxis() {
+    this.svg
+      .append('g')
+      .attr('class', 'y axis')
+      .call(this.yAxis);
+  }
+
 
 }
